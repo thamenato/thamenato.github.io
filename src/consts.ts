8f757bc89cef75bb19fcec4865ffcd3fb4429e01
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-01-16 22:32:07 -0500

    feat: update home

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Thales Menato",
+  NAME: "thamenato",
   EMAIL: "thales@thamenato.dev",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
